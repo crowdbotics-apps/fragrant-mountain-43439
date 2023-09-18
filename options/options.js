0fc6ff42a4f026185657bf58f2d5754d5c0f846d
Crowdbotics <team@crowdbotics.com>
2023-09-18 16:22:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-mountain-43439",
+  url: "https://fragrant-mountain-43439.botics.co",
+  api: "https://fragrant-mountain-43439.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
